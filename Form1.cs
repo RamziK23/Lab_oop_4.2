271a43d19fad2809230c46e6191984d46547ac78
RamziK23 <ramzesdavletbaev23@gmail.com>
2020-12-04 14:18:17 +0500

    V1.1

--- Form1.cs
+++ Form1.cs
@@ -23,45 +23,74 @@ namespace Lab_oop_4._2
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            model.setValue(Decimal.ToInt32(numericUpDown1.Value));
+            model.setValue(Convert.ToInt32(numericUpDown1.Value),0);
+
+            //if (numericUpDown1.Value % 2 == 1)
+            //    numericUpDown1.Value = numericUpDown1.Value + 1;
+            //progressBar1.Value = Decimal.ToInt32(numericUpDown1.Value);
+            //textBox1.Text = numericUpDown1.Value.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show(model.getValue().ToString());
+            //MessageBox.Show(numericUpDown1.Value.ToString());
         }
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
-                model.setValue(Int32.Parse(textBox1.Text));
+                model.setValue(Convert.ToInt32(textBox1.Text), 1);
+
+                //if (Int32.Parse(textBox1.Text) % 2 == 1)
+                //    textBox1.Text = (Int32.Parse(textBox1.Text) + 1).ToString();
+                //numericUpDown1.Value = Int32.Parse(textBox1.Text);
+                //progressBar1.Value = Int32.Parse(textBox1.Text);
+
             }
         }
         private void UpdateFromModel(object sender, EventArgs e) 
         {
-            textBox1.Text = model.getValue().ToString();
-            numericUpDown1.Value = model.getValue();
+           
             progressBar1.Value = model.getValue();
+            if (model.getCheck() == 0) 
+            {
+                numericUpDown1.Text = model.getValue().ToString();
+                textBox1.Text = (model.getValue() + 1).ToString();
+            }
+            else if (model.getCheck() == 1)
+            {
+                textBox1.Text = model.getValue().ToString();
+                numericUpDown1.Text = (model.getValue()-1).ToString();
+            } 
+
+
+               
+
+
         }
     }
 
     public class Model 
     {
         private int value;
-        public  System.EventHandler observers;
-
-        public void setValue(int value)
+        public int check;
+        public System.EventHandler observers;
+        public void setValue(int value, int check)
         {
-            if (value % 2 == 1)
-                this.value = value + 1;
-            else
-                this.value = value;
-            observers.Invoke(this,null);
+            this.value = value;
+            this.check = check;
+            observers.Invoke(this, null);
         }
-        public int getValue() 
+        public int getValue()
         {
             return value;
         }
+        public int getCheck()
+        {
+            return check;
+        }
+
     }
 }
